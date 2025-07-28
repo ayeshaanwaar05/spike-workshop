039fbfed6a4d1f00b736cff899f7062c8a8d3d60
ayesha <139478896+ayeshaanwaar05@users.noreply.github.com>
2025-07-28 19:46:07 +0500

    Update code.c

--- project/task_4/code.c
+++ project/task_4/code.c
@@ -1,1 +1,28 @@
 // Write your code here
+#include <stdio.h>
+
+// Function to compute x raised to the power n
+int power(int x, int n) {
+    int result = 1;
+
+    for (int i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+int main() {
+    int base, exponent;
+
+    printf("Enter base (x): ");
+    scanf("%d", &base);
+
+    printf("Enter exponent (n): ");
+    scanf("%d", &exponent);
+
+    int result = power(base, exponent);
+    printf("%d^%d = %d\n", base, exponent, result);
+
+    return 0;
+}
